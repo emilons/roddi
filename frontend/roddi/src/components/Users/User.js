2780502da13cc1b31efc4660aff3e67e567a2d73
Per Holte <phholte@stud.ntnu.no>
2021-02-12 15:44:01 +0100

    Added User #8

---
+++ frontend/roddi/src/components/Users/User.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+class User extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+            email: "",
+            password: "",
+        }
+
+        /* Funksjonen som sender dette til databasen: 
+        - Kjører et kall hvor man sjekker om verdier er tatt av andre brukere fra før.
+        */
+
+    }
+
+}
