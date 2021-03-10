d9bd4fb20e93f83fb2891be14efa5ac2f3f165a1
Emil <emilons@hotmail.com>
2021-03-10 17:14:19 +0100

    added MemberVotes component & GUI improvements #30

---
+++ frontend/roddi/src/components/MemberVotes.js
@@ -0,0 +1,29 @@
+import React, {useState, useEffect, Fragment} from "react";
+
+function MemberVotes(props) {
+    if (props.value > 0) {
+        return (
+            <Fragment>
+            <div className="voteDivide" style={{border: '1px solid', margin: "2px", backgroundColor: 'yellow'}}>Fordel</div>
+            <div className="voteDonate" style={{border: '1px solid', margin: "2px"}}>Doner</div>
+            <div className="voteTrash" style={{border: '1px solid', margin: "2px"}}>Kast</div>
+            </Fragment>
+        )}
+    else if (props.value == 0) {
+        return (
+            <Fragment>
+            <div className="voteDivide" style={{border: '1px solid', margin: "2px"}}>Fordel</div>
+            <div className="voteDonate" style={{border: '1px solid', margin: "2px", backgroundColor: 'yellow'}}>Doner</div>
+            <div className="voteTrash" style={{border: '1px solid', margin: "2px"}}>Kast</div>
+            </Fragment>
+        )}
+    else {
+        return (
+            <Fragment>
+            <div className="voteDivide" style={{border: '1px solid', margin: "2px"}}>Fordel</div>
+            <div className="voteDonate" style={{border: '1px solid', margin: "2px"}}>Doner</div>
+            <div className="voteTrash" style={{border: '1px solid', margin: "2px", backgroundColor: 'yellow'}}>Kast</div>
+            </Fragment>
+        )}
+}
+export default MemberVotes;
--- frontend/roddi/src/components/MyItem.js
+++ frontend/roddi/src/components/MyItem.js
@@ -1,21 +1,30 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, Fragment} from "react";
 import Estate from './Estate';
 import User from './User';
 import Item from './Item';
+import MemberVotes from './MemberVotes';
 import authService from '../services/auth.service';
 import tempImage from '../images/WIP.jpg';
 
 import '../App.css';
 
+
+
+
+
+
 function MyItem() {
+    const [isLoading, setIsLoading] = useState(true);
     const [estateID, setEstateID] = useState(1); // need function for getting ID
     const [itemID, setItemID] = useState(7); // need function for getting ID
     const [estateName, setEstateName] = useState("");
     const [item, setItem] = useState(new Item());
     const [user, setUser] = useState(new User()); // current logged in user
-    const [members, setMembers] = useState([]); // other family members
+    const [userItemChoice, setUserItemChoice] = useState(-1) // what current user voted on this item (update item.state.userChoice for this user based on this)
+    const [members, setMembers] = useState([]); // all family members
+    const [memberChoiceMap, setMemberChoiceMap] = useState(new Map())
 
-    useEffect(() => {
+    function initializeEstateAndMembers() {
         authService.getEstateFromID(estateID).then(res => {
             let tempEstate = new Estate();
             tempEstate.state = {
@@ -25,6 +34,7 @@ function MyItem() {
             members: res.data.users,
             }
             let initMembers = [];
+            let choiceMap = new Map()
             for (let i = 0; i < tempEstate.state.members.length; i++) {
                 let tempUser = new User();
                 tempUser.state = {
@@ -32,33 +42,45 @@ function MyItem() {
                     name: res.data.users[i].name,
                     email: res.data.users[i].email
                 }
+                choiceMap.set(tempUser.state.name, -1);
                 // if tempUser.name == currentUser.name then skip (so that current user is not in member list)
                 initMembers.push(tempUser);
             }
             let newMembers = members.concat(initMembers);
             setMembers(newMembers);
             setEstateName(res.data.name);
+            setMemberChoiceMap(choiceMap);
+            setIsLoading(false);
         })
+    }
 
-        //getCurrentUser -> setUser(currentUser)
-
+    function initializeItem() {
         authService.getItemByID(itemID).then(res => {
             let tempItem = new Item();
-            const choiceMap = new Map()
-            for (let i = 0; i < members.length; i++) {
-                choiceMap.set(members[i], -1);
-            }
             tempItem.state = {
                 id: res.data.id,
                 name: res.data.name,
                 description: res.data.description,
-                estate: res.data.estate,
-                userChoice: choiceMap
+                estate: res.data.estate
             }
             setItem(tempItem);
+            setIsLoading(false);
         })
+    }
 
+    useEffect(() => {
+        initializeEstateAndMembers();
+
+        //getCurrentUser -> setUser(currentUser)
+        initializeItem();
+        
+        
     }, []);
+
+    
+    function setVote(name, vote) {
+        
+    }
 
     return(
         <div className="MyItem">
@@ -80,14 +102,20 @@ function MyItem() {
                             <p>Comment...</p> 
                         </div>
                         <div className="userVotes">
-                        <div className="voteDivide" style={{border: '1px solid', margin: "2px"}}>Fordel</div>
-                        <div className="voteDonate" style={{border: '1px solid', margin: "2px"}}>Doner</div>
-                        <div className="voteTrash" style={{border: '1px solid', margin: "2px"}}>Kast</div>
-                            {/* 
-                            <div className="voteDivide"><input type="radio" value="Divide" name="vote"/> Fordel</div>
-                            <div className="voteDonate"><input type="radio" value="Donate" name="vote"/> Doner</div>
-                            <div className="voteTrash"><input type="radio" value="Trash" name="vote"/> Kast</div>
-                            */}
+                        {(!isLoading) ? <MemberVotes value={memberChoiceMap.get(element.state.name)}/> : <p>Loading...</p>}
+
+
+                        {/*element.state.userChoice[element.state.name] > 1 ? 
+                            <div className="voteDivide" style={{border: '1px solid', margin: "2px", backgroundColor: 'yellow'}}>Fordel</div>
+                        :
+                            <div className="voteDivide" style={{border: '1px solid', margin: "2px", backgroundColor: 'white'}}>Fordel</div>
+
+                            <div className="voteDonate" style={{border: '1px solid', margin: "2px"}}>Doner</div>
+                            <div className="voteTrash" style={{border: '1px solid', margin: "2px"}}>Kast</div>
+                                <div className="voteDivide"><input type="radio" value="Divide" name="vote"/> Fordel</div>
+                                <div className="voteDonate"><input type="radio" value="Donate" name="vote"/> Doner</div>
+                                <div className="voteTrash"><input type="radio" value="Trash" name="vote"/> Kast</div>
+                                */}
                         </div>
                     </div>
                 ))}
