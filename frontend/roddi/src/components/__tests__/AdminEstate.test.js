37d0966e13eb2b2e2883223047316c7a0fde8ae8
Victoria Hopso <victoria.hopso@gmail.com>
2021-03-22 13:49:52 +0100

    Some more basic rendering-tests (#34)

---
+++ frontend/roddi/src/components/__tests__/AdminEstate.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AdminEstates from '../AdminEstates';
+import {render, cleanup, screen, fireEvent} from '@testing-library/react'; 
+import '@testing-library/jest-dom';
+
+afterEach(cleanup);
+
+it("Renders Admins estate page", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<AdminEstates></AdminEstates>, div)
+});
+
+it("Renders correctly", () => {
+    const RenderResult = render(<AdminEstates />);
+    expect(RenderResult.container.getElementsByTagName("form")[0]).toBeDefined();
+    expect(RenderResult.container.getElementsByTagName("p")[0]).toBeDefined();
+    expect(RenderResult.container.getElementsByTagName("p")[0]).toHaveTextContent("ALLE DØDSBO:");
+    expect(RenderResult.container.getElementsByClassName("createEstate")).toBeDefined();
+    expect(RenderResult.container.getElementsByClassName("estates")).toBeDefined();
+    expect(RenderResult.container.getElementsByClassName("estateList")).toBeDefined();
+    expect(RenderResult.container.getElementsByClassName("btn btn-outline-danger")).toBeDefined();
+    expect(screen.getByPlaceholderText("Skriv inn navn på dødsbo")).toBeInTheDocument();
+});
