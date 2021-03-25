1efd11e26a878800b75c512277df4df33361a0c8
kristhho <kristhho@stud.ntnu.nogit config --global --listcat ~/.ssh/id_rsa.pubcat ~/.ssh/id_dsa.pubqquitgit>
2021-03-25 17:18:17 +0100

    #32 made table on statistics

--- frontend/roddi/src/components/Statistics.js
+++ frontend/roddi/src/components/Statistics.js
@@ -70,25 +70,42 @@ function Statistics() {
 
 
     return(<div>
-            <div>
-                <h1>Statistikk for Røddi</h1>
-            </div>
-            <div>
-                <h8>Dødsbo opprettet i dag:</h8> <h8 style = {{color: 'red'}}>{numberOfNewEstates}</h8>
-                <br></br>
-                <h8>Brukere registrert i dag:</h8> <h8 style = {{color: 'red'}}>{numberOfNewUsers}</h8>
-                <br></br>
-                <h8>Antall stemmer på eiendeler i dag: </h8> 
-                <h8 style = {{color: 'red'}}>{numberOfNewVotes}</h8>
+          
+            <div className="Statistic">
+              <div id="headerStatistics">
+                <h4>Statistikk for Røddi</h4>
+              </div>
+                <table id="t01"> 
+                  <tr>
+                    <th> </th>
+                    <th>Antall</th>
+                  </tr>
+                  <tr>
+                    <td id="second">Dødsbo opprettet idag</td>
+                    <td id="numbers1">{numberOfNewEstates}</td>
+                  </tr>
+                  <tr>
+                  <td>Brukere registrert idag</td>
+                    <td id="numbers">{numberOfNewUsers}</td>
+                  </tr>
+                  <tr>
+                  <td id="second">Antall stemmer på eiendeler i dag</td>
+                    <td id="numbers1">{numberOfNewVotes}</td>
+                  </tr>
+                  <tr>
+                  <td>Totalt antall dødsbo</td>
+                    <td id="numbers">{totalNumberOfEstates}</td>
+                  </tr>
+                  <tr>
+                  <td id="second">Totalt antall brukere</td>
+                    <td id="numbers1">{totalNumberOfUsers}</td>
+                  </tr>
+
+                </table>
             </div>
 
                 
-          <div>
-              <br></br> <br></br>
-              <h8>Totalt antall dødsbo:</h8> <h8 style = {{color: 'red'}}>{totalNumberOfEstates}</h8>
-              <br></br>
-              <h8>Totalt antall brukere:</h8> <h8 style = {{color: 'red'}}>{totalNumberOfUsers}</h8>
-            </div>
+          
         </div>);
   }
 
