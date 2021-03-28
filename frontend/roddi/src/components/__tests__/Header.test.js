c7d0ef103c8af51aec32e4004e2e1e48e102cae2
Per Holte <phholte@stud.ntnu.no>
2021-03-28 16:06:34 +0200

    Modified Header.js to get successful test

--- frontend/roddi/src/components/__tests__/Header.test.js
+++ frontend/roddi/src/components/__tests__/Header.test.js
@@ -4,3 +4,22 @@
  * 2) rendrer riktig 
  * 3) redirects funker
  */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from '../Header.js';
+import {render, cleanup, screen, fireEvent} from '@testing-library/react'; 
+import '@testing-library/jest-dom';
+
+afterEach(cleanup);
+
+it("Renders Header", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Header></Header>, div)
+});
+
+it("Header renders logo correctly", () => {
+    const RenderResult = render(<Header/>);
+    expect(screen.getByAltText("logoen")).toBeInTheDocument();
+})
+
