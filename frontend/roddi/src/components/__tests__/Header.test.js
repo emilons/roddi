121b1493342836b28086084103225ec16b1d2363
Per Holte <phholte@stud.ntnu.no>
2021-03-28 18:38:12 +0200

    Checking rendering for different users #34

--- frontend/roddi/src/components/__tests__/Header.test.js
+++ frontend/roddi/src/components/__tests__/Header.test.js
@@ -8,8 +8,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from '../Header.js';
-import {render, cleanup, screen, fireEvent} from '@testing-library/react'; 
+import {render, cleanup, screen} from '@testing-library/react'; 
 import '@testing-library/jest-dom';
+import { shallow } from 'enzyme';
 
 afterEach(cleanup);
 
@@ -23,3 +24,35 @@ it("Header renders logo correctly", () => {
     expect(screen.getByAltText("logoen")).toBeInTheDocument();
 })
 
+it("Renders correct buttons when logged in", () => {
+    const buttonsLoggedIn = [<button class="btn btn-outline-danger"><a href="#/MyEstates">Hjem</a></button>, <button class="btn btn-outline-danger">Logg Ut</button>]; 
+    localStorage.setItem('token', 'test');
+    localStorage.setItem('isAdmin', 'false');
+    const wrapper = render(<Header/>);
+    expect(wrapper.container.getElementsByClassName('btn btn-outline-danger').length).toEqual(2);
+    expect(wrapper.container.getElementsByClassName('btn btn-outline-danger')[0].firstElementChild.innerHTML).toEqual('Hjem');
+    expect(wrapper.container.getElementsByClassName('btn btn-outline-danger')[1].innerHTML).toEqual('Logg Ut');
+    localStorage.removeItem('token');
+    localStorage.removeItem('isAdmin');
+})
+
+it("Renders correct buttons when admin is logged in", () => {
+    const buttonsLoggedIn = [<button class="btn btn-outline-danger"><a href="#/MyEstates">Hjem</a></button>, <button class="btn btn-outline-danger">Logg Ut</button>]; 
+    localStorage.setItem('token', 'test');
+    localStorage.setItem('isAdmin', 'true');
+    const wrapper = render(<Header/>);
+    expect(wrapper.container.getElementsByClassName('btn btn-outline-danger').length).toEqual(3);
+    expect(wrapper.container.getElementsByClassName('btn btn-outline-danger')[0].firstElementChild.innerHTML).toEqual('Hjem');
+    expect(wrapper.container.getElementsByClassName('btn btn-outline-danger')[1].firstElementChild.innerHTML).toEqual('Statistikk');
+    expect(wrapper.container.getElementsByClassName('btn btn-outline-danger')[2].innerHTML).toEqual('Logg Ut');
+    localStorage.removeItem('token');
+    localStorage.removeItem('isAdmin');
+})
+
+it("Renders correct buttons when logged out", () => {
+    const buttonsLoggedIn = [<button class="btn btn-outline-danger"><a href="#/MyEstates">Hjem</a></button>, <button class="btn btn-outline-danger">Logg Ut</button>]; 
+    const wrapper = render(<Header/>);
+    expect(wrapper.container.getElementsByClassName('btn btn-outline-danger').length).toEqual(2);
+    expect(wrapper.container.getElementsByClassName('btn btn-outline-danger')[0].firstElementChild.innerHTML).toEqual('Registrer deg');
+    expect(wrapper.container.getElementsByClassName('btn btn-outline-danger')[1].firstElementChild.innerHTML).toEqual('Logg Inn');
+})
