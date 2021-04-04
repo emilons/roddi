e270cd2de5bbe2924a18ac060f06ca9605fd3b5f
Mikkel <mikkel.sandhaug@gmail.com>
2021-04-04 15:58:56 +0200

    Further documentation

--- backend/roddi/api/tests.py
+++ backend/roddi/api/tests.py
@@ -1,13 +1,15 @@
 from django.test import TestCase
-
-# Create your tests here.
-
-# Create your tests here.
-
 from django.test import TestCase
 from .models import Estate, Item, User_Item, User_In_Estate
 from django.core.exceptions import ObjectDoesNotExist
 
+
+### Contains automated unit tests for Estate and Item, other Inter-Models such as User-Item
+### and User-in-Estate are tested extensively with Postman. User model is django standard, does not
+### require our testing.
+
+
+#Automated unit testing for Estate model
 class EstateModelTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
@@ -27,19 +29,22 @@ class EstateModelTestCase(TestCase):
     def test_estate_existence(self):
         try: 
             Estate.objects.get(name="sandhaug")
-            self.fail("Should not exist estate with name sandhaug")
+            self.fail("Should not exist estate with name sandhaug") # "sandhaug" exists -> test fails
         except Estate.DoesNotExist:
             pass
 
         try:
             Estate.objects.get(id=3)
-            self.fail("Estate with ID 3 should not exist")
+            self.fail("Estate with ID 3 should not exist") # estate with id = 3 -> test fails
         except Estate.DoesNotExist:
             pass
 
-        
+
+#Automated unit testing for Item model      
 class ItemModelTestCase(TestCase):
 
+    #Creates two estates "andersen" and "jonsen" and two items "sofa" and "lampe"
+    #for testing purposes.
     @classmethod
     def setUpTestData(cls):
         Estate.objects.create(name="andersen", status=True)
@@ -50,9 +55,10 @@ class ItemModelTestCase(TestCase):
         Item.objects.create(name="lampe", description="ny og god", image="default",
         estate_id = 4)
 
+
     def test_item_attributes(self):
-        sofa = Item.objects.get(name="sofa")
-        lampe = Item.objects.get(name="lampe")
+        sofa = Item.objects.get(name="sofa") #If raises error -> test fails
+        lampe = Item.objects.get(name="lampe") #If raises error -> test fails
 
         self.assertEqual(sofa.id, 1)
         self.assertEqual(lampe.id, 2)
@@ -60,16 +66,17 @@ class ItemModelTestCase(TestCase):
         self.assertEqual(sofa.estate_id, 3)
         self.assertEqual(lampe.estate_id, 4)
 
+
     def test_item_existence(self):
         try: 
             Item.objects.get(name="bord")
-            self.fail("Should not exist item with name bord")
+            self.fail("Should not exist item with name bord") # "bord" exists -> test fails.
         except Item.DoesNotExist:
             pass
 
         try:
             Item.objects.get(id=3)
-            self.fail("Should not exist item with id 3")
+            self.fail("Should not exist item with id 3") # item with id = 3 -> test fails.
         except Item.DoesNotExist:
             pass
 
--- backend/roddi/roddi/asgi.py
+++ backend/roddi/roddi/asgi.py
@@ -1,11 +1,7 @@
-"""
-ASGI config for roddi project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
-"""
+#ASGI config for roddi project.
+#It exposes the ASGI callable as a module-level variable named ``application``.
+#For more information on this file, see
+#https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 
